4a6c2096bafd8d1c14713a888e2a9e21e68e4100
ShenghaoisYummy <hsupisces@hotmail.com>
2024-08-03 20:36:59 +1000

    add config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   css: {
     modules: {
       localsConvention: "camelCase",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        "/src/main.jsx", // Add the module that Rollup is failing to resolve
+      ],
     },
   },
 });
